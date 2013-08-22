38798324ce48d72115f599b27eceda8a652fb74f
Soledad Penades <sole@mozilla.com>
2013-08-22 17:59:21 +0100

    timeout, a bit

--- batch-download.js
+++ batch-download.js
@@ -5,7 +5,7 @@ var urls = JSON.parse(fs.readFileSync('appsURLs.json'));
 var currentURLIndex = null;
 var sys = require('sys');
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+function puts(error, stdout, stderr) { sys.puts(stdout); }
 
 downloadNext();
 
@@ -21,6 +21,13 @@ function downloadNext(error, stdout, stderr) {
     }
 
     if (currentURLIndex < urls.length) {
-        exec('casperjs download.js --url=' + urls[currentURLIndex], downloadNext);
+        var randomTimeout = 100000 * Math.random();
+        console.log('timeout ' + randomTimeout);
+        setTimeout(function() {
+            exec('casperjs download.js --url=' + urls[currentURLIndex], function(error, stdout, stderr) {
+                puts(error, stdout, stderr);
+                process.nextTick(downloadNext);
+            });
+        }, randomTimeout);
     }
 }

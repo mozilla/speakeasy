da7325603d48fe275fc1c24ae634204678a0570c
Soledad Penades <sole@mozilla.com>
2013-09-20 16:53:04 +0100

    Optionally capture screens (uncomment code in capture())

--- download.js
+++ download.js
@@ -4,9 +4,12 @@ var LOGIN_URL = 'https://accounts.google.com/ServiceLogin';
 var fs = require('fs');
 
 var casper = require('casper').create({
+    //waitTimeout: 1000,
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36',
     onWaitTimeout: function() {
         this.warn('Timeout for ' + this.getTitle() + '. Already installed?');
-        this.done();
+        capture(this);
+        this.exit();
     }
 });
 
@@ -19,14 +22,28 @@ try {
 }
 
 casper.start(LOGIN_URL, function() {
+    this.echo('started');
+    capture(this);
+});
+
+casper.waitForSelector('#gaia_loginform', function() {
     this.fill('#gaia_loginform', {
         Email: LOGIN_CREDENTIALS.email,
         Passwd: LOGIN_CREDENTIALS.password
     }, true);
 });
 
+casper.then(function() {
+    capture(this);
+});
+
 casper.thenOpen(url, function() {
+    capture(this);
     this.echo('Attempting to install app from ' + url);
+});
+
+casper.then(function() {
+    capture(this);
 });
 
 casper.waitForSelector('.buy-button-container .buy', function() {
@@ -42,3 +59,14 @@ casper.waitForSelector('.purchase-complete-container', function() {
 });
 
 casper.run();
+
+function capture(obj) {
+    /*
+    var title = obj.getTitle();
+    var d = new Date();
+    var outputFile = 'captures/000_screenshots/' + d.getTime() + '_' + (title.replace(/([^\w])+/g, '_')) + '.png';
+
+    obj.warn('Capturing to ' + outputFile);
+    obj.capture(outputFile, undefined, { format: 'png' });
+    */
+}
